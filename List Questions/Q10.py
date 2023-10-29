6a404825141534a8a886e607b2fd2dcc17b1f7c7
Shraddha Khot <shraddha4320@gmail.com>
2023-10-29 21:02:07 +0530

    Updating Q10.py

--- List Questions/Q10.py
+++ List Questions/Q10.py
@@ -1,5 +1,5 @@
 '''
-10. Write a Python program to find the list of words that are longer than n from a given list of 
+10. Write a Python   program to find the list of words that are longer than n from a given list of 
 words. 
 '''
 mylist=[]
